fca25d4647adb6298e465f368a976591e09dbe59
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-02-27 08:50:15 +0000

    added 'special attack' button methods and computed

--- app.js
+++ app.js
@@ -7,19 +7,39 @@ const app = Vue.createApp({
         return {
             playerHealth: 100, 
             monsterHealth: 100,
+            currentRound: 0
         }
     }, 
     methods: {
         attackMonster() {
-            console.log(this.monsterHealth);
+            console.log('Monster Health: ' + this.monsterHealth);
+            console.log('Round: ' + this.currentRound);
             const attackValue = getRandomNumber(5, 12);
             this.monsterHealth = this.monsterHealth - attackValue;
+            this.currentRound++;
             this.attackPlayer();
         }, 
         attackPlayer() {
-            console.log(this.playerHealth);
+            console.log('Player Health: ' + this.playerHealth);
             const attackValue = getRandomNumber(8, 15);
             this.playerHealth = this.playerHealth - attackValue;
+        }, 
+        specialAttackMonster() {
+            const attackValue = getRandomNumber(10, 25);
+            this.monsterHealth = this.monsterHealth - attackValue;
+            this.currentRound ++;
+            this.attackPlayer();
+        }
+    }, 
+    computed: {
+        monsterBarStyles() {
+            return {width: this.monsterHealth + '%'};
+        }, 
+        playerBarStyles() {
+            return {width: this.playerHealth + '%'};
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 != 0;
         }
     }
 })
